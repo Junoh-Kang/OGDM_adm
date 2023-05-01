3ebdbb0ebf0c22561ef25f19ab6d167a3c63234c
junoh <junoh.kang@snu.ac.kr>
2023-05-01 17:51:19 +0900

    cifar10 finetune config change

--- eval_fid.sh
+++ eval_fid.sh
@@ -6,13 +6,13 @@ read -p "Enter the GPU number to use: " gpu
 
 
 echo $dir2 >> eval/fid/$file
-# output=$(CUDA_VISIBLE_DEVICES=$gpu python -m pytorch_fid $dir1 $dir2/ddim100 --batch-size 512)
-# echo ddim100 : $output >> eval/fid/$file
-# output=$(CUDA_VISIBLE_DEVICES=$gpu python -m pytorch_fid $dir1 $dir2/ddim50 --batch-size 512)
-# echo ddim50 : $output >> eval/fid/$file
+output=$(CUDA_VISIBLE_DEVICES=$gpu python -m pytorch_fid $dir1 $dir2/ddim100 --batch-size 512)
+echo ddim100 : $output >> eval/fid/$file
+output=$(CUDA_VISIBLE_DEVICES=$gpu python -m pytorch_fid $dir1 $dir2/ddim50 --batch-size 512)
+echo ddim50 : $output >> eval/fid/$file
 output=$(CUDA_VISIBLE_DEVICES=$gpu python -m pytorch_fid $dir1 $dir2/ddim20 --batch-size 512)
 echo ddim20 : $output >> eval/fid/$file
 output=$(CUDA_VISIBLE_DEVICES=$gpu python -m pytorch_fid $dir1 $dir2/ddim10 --batch-size 512)
 echo ddim10 : $output >> eval/fid/$file
-# output=$(CUDA_VISIBLE_DEVICES=$gpu python -m pytorch_fid $dir1 $dir2/ddim5 --batch-size 512)
-# echo ddim5 : $output >> eval/fid/$file
+output=$(CUDA_VISIBLE_DEVICES=$gpu python -m pytorch_fid $dir1 $dir2/ddim5 --batch-size 512)
+echo ddim5 : $output >> eval/fid/$file
